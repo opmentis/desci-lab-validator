aef8bb1d1739a4655aac0b9fe5b13528e6d19411
opmentis <opmentisai@gmail.com>
2024-12-19 18:09:44 +0100

    Validator

--- register.py
+++ register.py
@@ -1,7 +1,7 @@
 from opmentis import register_user
 
 # Register as a miner
-wallet_address = "0xee0495497e6847e3F85343B6474A0658c32F51dd"
+wallet_address = "wallet_address"
 labid = "dbc00e29-721f-40e6-b073-ec627db90115"
 role_type = "validator"
 register_response = register_user(wallet_address, labid, role_type)
